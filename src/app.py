c9dd602aa857902f2d55498b8f2353640139eff6
MuriloRohor <murilo.marquezini@gmail.com>
2023-10-04 19:08:00 -0300

    Reformulando o diretorio e aplicando o template do jinja

--- src/app.py
+++ src/app.py
@@ -1,15 +1,19 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
-from routers import web_routes
+from fastapi.templating import Jinja2Templates
 import uvicorn
 
 app = FastAPI()
 
-# Montando arquivos estáticos
-app.mount("/static", StaticFiles(directory="../public/static"), name="static")
+templates = Jinja2Templates(directory="public/templates")
 
-# Incluindo rotas de renderização
-app.include_router(web_routes.router)
+app.mount("/static", StaticFiles(directory="public/static"), name="static")
+
+
+@app.get("/", response_class=HTMLResponse)
+async def login_page(request: Request):
+    return templates.TemplateResponse("login.html", {"request": request})
 
 if __name__ == "__main__":
     uvicorn.run(app="app:app", host="localhost", port=8000, reload=True)
