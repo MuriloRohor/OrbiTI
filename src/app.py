d318f4eb092f72a2cb8066fef8bc07ae5637e1b2
MuriloRohor <murilo.marquezini@gmail.com>
2023-10-08 19:08:49 -0300

    router_pages

--- src/app.py
+++ src/app.py
@@ -1,19 +1,14 @@
 from fastapi import FastAPI, Request
-from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+from routers.routerPage import router_page
 import uvicorn
 
 app = FastAPI()
 
-templates = Jinja2Templates(directory="public/templates")
-
 app.mount("/static", StaticFiles(directory="public/static"), name="static")
 
-
-@app.get("/", response_class=HTMLResponse)
-async def login_page(request: Request):
-    return templates.TemplateResponse("login.html", {"request": request})
+app.include_router(router_page)
 
 if __name__ == "__main__":
     uvicorn.run(app="app:app", host="localhost", port=8000, reload=True)
---
+++ src/routers/routerPage.py
@@ -0,0 +1,19 @@
+from fastapi import Request, APIRouter
+from fastapi.responses import HTMLResponse, RedirectResponse
+from fastapi.templating import Jinja2Templates
+
+router_page = APIRouter()
+templates = Jinja2Templates(directory="public/templates")
+
+
+@router_page.get("/", response_class=HTMLResponse)
+def menu_page(request: Request):
+    return RedirectResponse(url="/login")
+
+@router_page.get("/login", response_class=HTMLResponse)
+def login_page(request: Request):
+    return templates.TemplateResponse("login.html", {"request": request})
+
+@router_page.get('/menu', response_class=HTMLResponse)
+def menu_page(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
