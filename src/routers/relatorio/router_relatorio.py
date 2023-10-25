1aec4b3d08cdb2246c64a75d91fee2530e7bffd9
MuriloRohor <murilo.marquezini@gmail.com>
2023-10-25 19:09:08 -0300

    tela principal do relatorio refeita

--- src/routers/relatorio/router_relatorio.py
+++ src/routers/relatorio/router_relatorio.py
@@ -8,8 +8,10 @@ router = APIRouter()
 
 @router.get('/', response_class=HTMLResponse)
 def relatorio_main(request: Request):
-    return templates.TemplateResponse("relatorio/main.html", {"request": request})
+    titulo_page = "Dashboard"
+    return templates.TemplateResponse("relatorio/main.html", {"request": request, "titulo": titulo_page})
 
 @router.get('/estoque', response_class=HTMLResponse)
 def relatorio_main(request: Request):
-    return templates.TemplateResponse("relatorio/estoque.html", {"request": request})
+    titulo_page = "Dashboard Estoque"
+    return templates.TemplateResponse("relatorio/estoque.html", {"request": request, "titulo":titulo_page})
