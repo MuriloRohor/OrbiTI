bda8285d86a1c124e226a5693114420f405d80a6
MuriloRohor <murilo.marquezini@gmail.com>
2023-12-04 21:48:05 -0300

    refatorando tests

--- src/tests/test_db.py
+++ src/tests/test_db.py
@@ -12,6 +12,7 @@ def test_create_endereco(session):
         numero=1
     )
     session.add(new_endereco)
+    session.commit()
 
     endereco = session.scalar(
         select(Endereco).where(
@@ -29,25 +30,30 @@ def test_create_fornecedor(session):
         bairro="teste",
         numero=1
     )
-    session.add(new_endereco_for_fornecedor)
-    endereco = session.scalar(
+    session.add(new_endereco_for_fornecedor)     # Adiciona o objeto criado na sessão.
+    session.commit()                             # Faz o commit dos dados para o banco.
+    endereco_id = new_endereco_for_fornecedor.id # Recupera o Id do endereço criado.
+    endereco = session.scalar(                   # Recupera o endereço se a condição for satisfeita.
         select(Endereco).where(
             Endereco.cidade == "joeba"
         )
     )
 
+
     new_fornecedor = Fornecedor(
         nome="pedro",
         cnpj=1234567891000,
         telefone=123456789,
-        endereco_id=1
+        endereco_id=endereco_id                 # Informando o id do endereco recem criado
     )
     session.add(new_fornecedor)
-    fornecedor = session.scalar(
+    session.commit()
+    fornecedor = session.scalar(                # Recupera o fornecedor se o id do endereço for igual ao endereço recem criado
         select(Fornecedor).where(
-            Fornecedor.endereco_id == 1
+            Fornecedor.endereco_id == endereco_id
         )
     )
-    assert fornecedor.id == 1
+    assert fornecedor is not None
+    assert fornecedor.endereco_id == endereco_id
     assert endereco.cidade == "joeba"
     
