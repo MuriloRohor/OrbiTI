5a7ce4a9f55135c33e38d38ccfd6c8fa3713e44f
MuriloRohor <murilo.marquezini@gmail.com>
2023-12-06 11:21:26 -0300

    add schema usuario

---
+++ src/schemas/UsuarioSchema.py
@@ -0,0 +1,20 @@
+from pydantic import BaseModel
+from typing import Optional
+
+from .EnderecoSchema import EnderecoOut
+from .PermissaoUsuarioSchema import PermissaoUsuarioOut
+
+class UsuarioOut:
+    id: Optional[int] = None
+    nome: str
+    sobrenome: str
+    data_nascimento: str
+    cpf: int
+    cargo: str
+    login: str
+    senha: str
+    endereco_id: int
+    permissao_id: id
+    permissao: Optional[PermissaoUsuarioOut] = None
+    endereco: Optional[EnderecoOut] = None
+    
